613eef0be2f18a9c5cdd8926237a202b58cf8164
ralf <ralf.wirdemann@gmail.com>
2020-10-09 16:52:19 +0200

    Initial Commit

---
+++ agenda/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  agenda
+//
+//  Created by Ralf Wirdemann on 09.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
