5c027640f159fd604e4f32612cef3cd3d7b0e83d
ralf <ralf.wirdemann@gmail.com>
2020-10-11 17:24:24 +0200

    Adds dynamic testdata

--- agenda/ContentView.swift
+++ agenda/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    let lists = TestData.lists()
+    
     var body: some View {
         NavigationView {
              List {
-                NavigationLink(destination: DetailsView(list: "Inbox")) {
-                    Text("Inbox")
-                 }
-                NavigationLink(destination: DetailsView(list: "Today")) {
-                    Text("Today")
+                ForEach(lists) { list in
+                    NavigationLink(destination: DetailsView(list: list)) {
+                        Text("\(list.name)")
+                     }
                 }
              }
              .listStyle(SidebarListStyle())
@@ -24,11 +25,48 @@ struct ContentView: View {
     }
 }
 
-struct DetailsView: View {
-    let list: String
+struct ItemList: Identifiable {
+    var id: String = UUID().uuidString
+    
+    let name: String
+    var items: [Item] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Item: Identifiable {
+    var id: String = UUID().uuidString
+    
+    let description: String
+    
+    init(description: String) {
+        self.description = description
+    }
+}
+
+struct ItemView: View {
+    let item: Item
+    
     var body: some View {
-        VStack {
-            Text(list)
+        HStack(spacing: 10) {
+            Text(item.description)
+        }
+    }
+    
+    init(item: Item) {
+        self.item = item
+    }
+}
+
+struct DetailsView: View {
+    let list: ItemList
+    var body: some View {
+        List  {
+            ForEach(list.items) { item in
+                ItemView(item: item)
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
@@ -37,5 +75,23 @@ struct DetailsView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TestData {
+    static func lists() -> [ItemList] {
+        var inbox = ItemList(name: "Inbox")
+        inbox.items.append(Item(description: "Mit Tobi telefonieren"))
+        inbox.items.append(Item(description: "Handschuhe kaufen"))
+
+        var today = ItemList(name: "Heute")
+        today.items.append(Item(description: "Evgeny anrufen"))
+        today.items.append(Item(description: "Intersoft Herkules Meeting"))
+
+        var planned = ItemList(name: "Geplant")
+        planned.items.append(Item(description: "Coma in Fitness umbennen"))
+        planned.items.append(Item(description: "Beschreibung ausliefern"))
+
+        return [inbox, today, planned]
     }
 }
